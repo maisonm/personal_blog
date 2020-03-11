1ef7ebd5a62650cd858951b685cc1c3b25312c2b
Maison Moa <maison.moa@gmail.com>
2020-03-11 18:38:36 -0400

    main components setup for dashboard, including side navigation, side nav view selection and the dashboard component itself

--- client/src/components/Icons/styles.js
+++ client/src/components/Icons/styles.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
 
-export const IconSpan = styled.span`
+export const IconContainer = styled.div`
   font-size: ${props => props.size};
-  color: ${props => props.theme[props.color]};
+  color: ${props =>
+    props.active ? props.theme[props.hovercolor] : props.theme[props.color]};
+  cursor: ${props => (props.cursor ? props.cursor : null)};
+  transition: ${props => props.theme.hoverTrans};
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  &:hover {
+    transition: ${props => props.theme.hoverTrans};
+    color: ${props =>
+      props.hoverColor ? props.hovercolor : props.theme.secondaryIconColor};
+  }
+
+  & > p {
+    font-size: 0.65em;
+    font-weight: 500;
+    margin-left: 8px;
+  }
 `;
--- client/src/fontawesome_library/fontawesome.js
+++ client/src/fontawesome_library/fontawesome.js
@@ -4,6 +4,22 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 // import { faEdit, faPen } from '@fortawesome/free-solid-svg-icons';
 
-import { faEdit, faStickyNote } from '@fortawesome/free-regular-svg-icons';
+import {
+  faEdit,
+  faPlusSquare,
+  faPlayCircle,
+  faStickyNote,
+  faListAlt,
+  faChartBar,
+  faArrowAltCircleRight
+} from '@fortawesome/free-regular-svg-icons';
 
-library.add(faEdit, faStickyNote);
+library.add(
+  faEdit,
+  faStickyNote,
+  faPlusSquare,
+  faPlayCircle,
+  faListAlt,
+  faChartBar,
+  faArrowAltCircleRight
+);
