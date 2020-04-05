f0b169e1731be720301bd21aabf66764b296965f
Maison Moa <maison.moa@gmail.com>
2020-04-05 11:10:25 -0400

    updated client build

---
+++ client/build/precache-manifest.e630072a6769a52f2225eca6a59e83cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445250a778f68fc5faa78dbfa4bf7ffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9488965cbd497215918a",
+    "url": "/static/css/main.249ef210.chunk.css"
+  },
+  {
+    "revision": "41a83fe1300cb9d0fd17",
+    "url": "/static/js/2.09cde562.chunk.js"
+  },
+  {
+    "revision": "d9bee4e26266dab8796c9ed1573a3e49",
+    "url": "/static/js/2.09cde562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9488965cbd497215918a",
+    "url": "/static/js/main.9b68d4ad.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "d54bf380466ff797559fe79610a2b146",
+    "url": "/static/media/bolt.d54bf380.svg"
+  }
+]);
